98317077e3c7a290964f6a7c20996763824cfd60
codcodog <codcodog@gmail.com>
2018-11-22 20:56:13 +0000

    awesome wm: 启动执行扩展屏幕脚本

--- rc.lua
+++ rc.lua
@@ -563,3 +563,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- 自定义启动命令
+awful.util.spawn_with_shell('~/screen.sh')
