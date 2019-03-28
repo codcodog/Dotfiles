9746ee80adba052d4a19d30f648341f0283bb2b1
codcodog <codcodog@gmail.com>
2019-03-28 22:27:41 +0800

    移除 screen 脚本

--- screen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if xrandr | grep -q '^DP1 connected'; then
-  xrandr --output VIRTUAL1 --off --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --off --output DP2 --off
-fi
